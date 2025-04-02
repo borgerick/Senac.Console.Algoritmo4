c6ade6c59fb4b84d82c197900197679924543b9e
Erick Borges Rodrigues <borges.ericks@gmail.com>
2025-04-01 21:36:19 -0300

    Conclusão exercicio

---
+++ Algoritmo4/Algoritmo4/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System.Globalization;
+
+// Variaveis
+double largura, comprimento, area, valorMetro, valorTerreno;
+
+//Entrada
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+// Calculo
+area = largura * comprimento;
+valorTerreno = area * valorMetro;
+
+// Saida (resultado do calulo ou mensagem)
+Console.WriteLine("AREA - " + area.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = " + valorTerreno.ToString("F2", CultureInfo.InvariantCulture));
